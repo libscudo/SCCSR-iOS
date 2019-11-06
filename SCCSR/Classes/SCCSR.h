a3e2894c9ae597435c901e4d7ef9ecc13380c182
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2019-11-06 07:30:16 -0300

    wip

---
+++ SCCSR/Classes/SCCSR.h
@@ -0,0 +1,17 @@
+//
+//  SCCSR.h
+//  SCCSR
+//
+//  Created by Kimi on 06/11/2019.
+//
+
+#import <Foundation/Foundation.h>
+#import "OpenSSL/include/ssl.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SCCSR : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SCCSR/Classes/SCCSR.m
@@ -0,0 +1,12 @@
+//
+//  SCCSR.m
+//  SCCSR
+//
+//  Created by Kimi on 06/11/2019.
+//
+
+#import "SCCSR.h"
+
+@implementation SCCSR
+
+@end
