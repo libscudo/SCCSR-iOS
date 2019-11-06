da8de146a7ebc2c0619eb91c1e2fbaef23c035e8
Ezequiel Aceto <eaceto@afip.gob.ar>
2019-11-06 14:33:43 -0300

    Add test and examples

---
+++ Example/Pods/Target Support Files/Pods-SCCSR_Tests/Pods-SCCSR_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SCCSR_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SCCSR_Tests
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,48 +3,45 @@
 import Quick
 import Nimble
 import SCCSR
+import Security
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
         describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
+            
+            var statusCode: OSStatus
+            var publicKey: SecKey?
+            var privateKey: SecKey?
+                                
+            let publicKeyAttr: [String: Any] = [kSecAttrIsPermanent as String:true, kSecAttrApplicationTag as String:"publicTag"]
+            let privateKeyAttr: [String: Any] = [kSecAttrIsPermanent as String:true, kSecAttrApplicationTag as String:"privateTag"]
+            
+            var keyPairAttr = [String: Any]()
+            keyPairAttr[kSecAttrKeyType as String] = kSecAttrKeyTypeRSA
+            keyPairAttr[kSecAttrKeyType as String] = kSecAttrKeyTypeRSA
+            keyPairAttr[kSecAttrKeySizeInBits as String] = 2048
+            keyPairAttr[kSecPublicKeyAttrs as String] = publicKeyAttr
+            keyPairAttr[kSecPrivateKeyAttrs as String] = privateKeyAttr
+            
+            statusCode = SecKeyGeneratePair(keyPairAttr as CFDictionary, &publicKey, &privateKey)
+            
+            if statusCode == noErr {
+                guard let publicKey = publicKey, let privateKey = privateKey else {
+                    expect(0) == 1
+                    return
+                }
+                
+                let csrAttributes = SCCSRAttributes(commonName: "App", organization: "Organization", organizationUnit: "IT", country: "AR", stateOrProvince: "Ciudad de Buenos Aires", locality: "Ciudad de Buenos Aires", serialNumber: "SN 1234567890")
+                
+                SCCSRHelper.generateCSR(from: privateKey, publicKey: publicKey, attributes: csrAttributes, with: KeyAlgorithm.rsa(signatureType: .sha256)) { csr, error in
+                    expect(error).to(beNil())
+                    expect(csr).notTo(beNil())
+                }
+                
+            } else {
+                expect(0) == 1
             }
             
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
         }
     }
 }
